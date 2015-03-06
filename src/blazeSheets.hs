5b95c7cccd25cb8cdc59ef8146a1d8a1f9a6e3d9
Wallace <lukwalllace@gmail.com>
2015-03-05 22:05:10 -0800

    runnable changes added

--- src/blazeSheets.hs
+++ src/blazeSheets.hs
@@ -7,19 +7,19 @@ import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Utf8 as R
 import qualified Data.ByteString.Lazy as L
 import System.Environment
-import Data.Map
+import Data.Map as D
 import MusicSheet
 
 --For keeping track of data maybe? Or if we could learn to do w/ monads
 --The first int is the Y position, second is X position, and the String
 --represents the flats and sharps in the key signature
 data Manager = M Int Int String
-type noteMap = Map Float Int
+type NoteMap = Map Float Int
 
 
 initX = 0
 initY = 0
-sizeOfStanza
+sizeOfStanza = 0
 halfOfStanza = 700
 sizeOfLetter = 12
 
@@ -44,83 +44,100 @@ setupString = "#container{height:2300px;width:3000px;position:relative;}" ++
 			  "#line{z-index:100;position:absolute;height: 50px;width: 50px;}"
 
 sheetHtml :: Int -> Int -> Html
-sheets a b = H.img ! A.style (toValue(str)) ! A.id "image" ! A.src "img/newSheet.png"
+sheetHtml a b = H.img H.! A.style (toValue(str)) H.! A.id "image" H.! A.src "img/newSheet.png"
     		 where str = "top:" ++ show a ++ "px; left:" ++ show b ++ "px;"
 
 
 titleHtml :: String -> Html
-titleHtml xs =  H.h1 ! A.style (toValue("top:0px; left:" ++ show a ++ "px;")) ! A.id "title" $ toHtml xs
+titleHtml xs =  H.h1 H.! A.style (toValue("top:0px; left:" ++ show a ++ "px;")) H.! A.id "title" $ toHtml xs
 				where a = halfOfStanza - (0.5*(fromIntegral(length xs))*sizeOfLetter)
 
 unitHtml :: String -> Int -> Int -> Html
 unitHtml x a b
-    | x == "fourth"      = H.img ! A.style (toValue(str)) ! A.id "fourth" ! A.src "img/4th-note.png"
-    | x == "eighth"      = H.img ! A.style (toValue(str)) ! A.id "eighth" ! A.src "img/8th-note.png"
-    | x == "sixteen"     = H.img ! A.style (toValue(str)) ! A.id "sixteen" ! A.src "img/16th-note.png"
-    | x == "whole"       = H.img ! A.style (toValue(str)) ! A.id "whole" ! A.src "img/whole-note.png"
-    | x == "half"        = H.img ! A.style (toValue(str)) ! A.id "half" ! A.src "img/half-note.png"
-    | x == "wholerest"   = H.img ! A.style (toValue(str)) ! A.id "wholerest" ! A.src "img/wholerest.png"
-    | x == "halfrest"    = H.img ! A.style (toValue(str)) ! A.id "halfrest" ! A.src "img/halfrest.png"
-    | x == "forth-rest"  = H.img ! A.style (toValue(str)) ! A.id "forth-rest" ! A.src "img/4th-rest.png"
-    | x == "eightrest"   = H.img ! A.style (toValue(str)) ! A.id "eightrest" ! A.src "img/eightrest.png"
-    | x == "flag"        = H.img ! A.style (toValue(str)) ! A.id "flag" ! A.src "img/flag.png"
-    | x == "natural"     = H.img ! A.style (toValue(str)) ! A.id "natural" ! A.src "img/natural.png"
-    | x == "sharp"       = H.img ! A.style (toValue(str)) ! A.id "sharp" ! A.src "img/sharp.png"
-    | x == "commontime"  = H.img ! A.style (toValue(str)) ! A.id "commontime" ! A.src "img/commontime.png"
-    | x == "line"        = H.img ! A.style (toValue(str)) ! A.id "line" ! A.src "img/line.png"
+    | x == "fourth"      = H.img H.! A.style (toValue(str)) H.! A.id "fourth" H.! A.src "img/4th-note.png"
+    | x == "eighth"      = H.img H.! A.style (toValue(str)) H.! A.id "eighth" H.! A.src "img/8th-note.png"
+    | x == "sixteen"     = H.img H.! A.style (toValue(str)) H.! A.id "sixteen" H.! A.src "img/16th-note.png"
+    | x == "whole"       = H.img H.! A.style (toValue(str)) H.! A.id "whole" H.! A.src "img/whole-note.png"
+    | x == "half"        = H.img H.! A.style (toValue(str)) H.! A.id "half" H.! A.src "img/half-note.png"
+    | x == "wholerest"   = H.img H.! A.style (toValue(str)) H.! A.id "wholerest" H.! A.src "img/wholerest.png"
+    | x == "halfrest"    = H.img H.! A.style (toValue(str)) H.! A.id "halfrest" H.! A.src "img/halfrest.png"
+    | x == "forth-rest"  = H.img H.! A.style (toValue(str)) H.! A.id "forth-rest" H.! A.src "img/4th-rest.png"
+    | x == "eightrest"   = H.img H.! A.style (toValue(str)) H.! A.id "eightrest" H.! A.src "img/eightrest.png"
+    | x == "flag"        = H.img H.! A.style (toValue(str)) H.! A.id "flag" H.! A.src "img/flag.png"
+    | x == "natural"     = H.img H.! A.style (toValue(str)) H.! A.id "natural" H.! A.src "img/natural.png"
+    | x == "sharp"       = H.img H.! A.style (toValue(str)) H.! A.id "sharp" H.! A.src "img/sharp.png"
+    | x == "commontime"  = H.img H.! A.style (toValue(str)) H.! A.id "commontime" H.! A.src "img/commontime.png"
+    | x == "line"        = H.img H.! A.style (toValue(str)) H.! A.id "line" H.! A.src "img/line.png"
     where str = "top:" ++ show a ++ "px; left:" ++ show b ++ "px;"
 
 
 --unfinished, will need more parameters for flat/sharp checking?
-soundToHtml :: Sound -> Int -> Int -> Html
+{-soundToHtml :: Sound -> Int -> Int -> Html
 soundToHtml s y x = case s of
 	(Note None n d o) -> unitHtml (findUnitType d n) (yPos n o y) (xPos d x)
 	(Note Flat n d o) -> do (makeFlat);
 							unitHtml (findUnitType d n) (yPos n o y) (xPos d x)
 	(Note Sharp n d o) -> do (makesharp);
 							 unitHtml (findUnitType d n) (yPos n o y) (xPos d x)
-
+-}
 --for testing
 test :: String -> Html
 test x = docTypeHtml $ do
 	H.head $ do
-		H.meta ! A.charset "uft-8"
+		H.meta H.! A.charset "uft-8"
 		H.style $ toHtml setupString
 	H.body $ do
-		H.div ! A.id "container" $ do (sheetHtml 0 0);
-									  (titleHtml x);
+		H.div H.! A.id "container" $ do (sheetHtml 0 0);
+									  	(titleHtml x);
 
 makeSheet :: Sheet -> Html
 makeSheet (Sheet t fs s) = docTypeHtml $ do
 	H.head $ do
-		H.meta ! A.charset "uft-8"
+		H.meta H.! A.charset "uft-8"
 		H.style $ toHtml setupString
 	H.body $ do
-		H.div ! A.id "container" $ do (sheetHtml 0 0);
+		H.div H.! A.id "container" $ do (sheetHtml 0 0);
 									  (titleHtml t);
 									  (songHtml (newManager fs) s);
 
 songHtml :: Manager -> [[[Sound]]] -> Html
-songHtml m@(M y x fs) xsss = do fshtml
-							  	musicHtml newm xsss
+songHtml m@(M y x fs) xsss = do fshtml;
+							  	(musicHtml newm xsss);
 							where (fshtml, newm) = keySigHtml m xsss
 
 newManager :: String -> Manager
-newManager fs = Manager initY initX fs
+newManager fs = (M initY initX fs)
 
 yInc :: Int -> Int
 yInc y = y + sizeOfStanza
 
 
 musicHtml :: Manager -> [[[Sound]]] -> Html
-musicHtml m@(M y x fs) (s:ss) = (printStanza m s) ++ musicHtml (M (yInc y) x fs) ss
+musicHtml m@(M y x fs) (s:ss) = do (printStanza m s);
+									musicHtml (M (yInc y) x fs) ss
 
+printStanza :: Manager -> [[Sound]] -> Html
+printStanza m@(M y x fs) (s:ss) = "Nope"
 
 keySigHtml :: Manager -> [[[Sound]]] -> (Html, Manager)
-keySigHtml m xsss = foldl (makeKeySig) ("",m) xsss
+keySigHtml m xsss = Prelude.foldl (makeKeySig) ("",m) xsss
 
 makeKeySig :: (Html, Manager) -> [[Sound]] -> (Html, Manager)
-makeKeySig (h, (M y x fs)) _ = 
+makeKeySig (h, (M y x fs)) anything = (nh, (M ny x fs))
+		where ny = y + sizeOfStanza;
+			  nh = do h;
+			          (printKeySig fs y x);
+
+printKeySig :: String -> Int -> Int -> Html
+printKeySig fs x y = case (Prelude.head fs) of
+			   			('f') -> flatHtml (tail fs) y x
+			  			('s') -> sharpHtml (tail fs) y x
+
+flatHtml :: String -> Int -> Int -> Html
+flatHtml fs y x = "Nope"
+
+sharpHtml :: String -> Int -> Int -> Html
+sharpHtml fs y x  = "Nope"
 
 
 main = do
